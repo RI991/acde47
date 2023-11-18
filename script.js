8d1eb453dc4024ac269596d35f2ea46e4ccce7df
RI991 <150167549+RI991@users.noreply.github.com>
2023-11-18 23:30:10 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add JavaScript functionalities here, like form validation, interactive elements, etc.
+// Example: Form Validation
+const form = document.querySelector('.contact-form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  // Validation logic and submission handling
+  // Example: Check if fields are filled properly before submission
+});
